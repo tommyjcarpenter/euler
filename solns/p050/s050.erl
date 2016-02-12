39ce7b6d58b5804a5e76a2ed1b5a7c481d16702f
Tommy Carpenter <tommyjcarpenter@gmail.com>
2016-02-12 11:17:37 -0500

    50 done, but is SLOW, could be better. Takes two hours to get answer

--- solns/p050/s050.erl
+++ solns/p050/s050.erl
@@ -11,22 +11,24 @@
 -module(s050).
 -export([solve/0]).
 
-sublistsum(L, Start, N) -> lists:sum(lists:sublist(L, Start, N)).
-
 %returns the largest sublist (in terms of length) of L that sums to Target, or 0 if not possible
-largest_consec_from_build_launcher(L, Target) -> largest_consec_from_build(L, 1, 1, Target).
-largest_consec_from_build(L, Start, N, Target) ->
-    if Start > length(L) orelse N  > length(L)-Start -> 0;
+largest_consec_from_build(A, Size, Start, N, Acc, Target) ->
+    if Start + N > Size-1  -> 0; %primes monotonucally increasing, so if H > T/2, then H + Hnext > Target 
     true ->
-        S =  sublistsum(L, Start, N), 
-        if S == Target -> N;
+        S = Acc + array:get(Start+N, A), 
+        %sublistsum(L, N), %I believe the first substring of primes that sums to the target is necessarily the longest. Because the primes are monotonically increasing, any substring summing to the same amount with a higher start index necessarily has les terms. So, return the first substring that sums to the target. 
+        if S == Target -> {N, Target};
         true ->
-            if S > Target -> largest_consec_from_build(L, Start+1, 1, Target);
-            true -> largest_consec_from_build(L, Start, N+1, Target)
+            if S > Target -> largest_consec_from_build(A, Size, Start+1, 1,   0, Target);
+            true ->          largest_consec_from_build(A, Size, Start,   N+1, S, Target)
             end
         end
     end.
 
 solve() ->
-    largest_consec_from_build_launcher(shared_euler:seive(953), 953).
+    Ps = shared_euler:seive(999999),
+    A = array:from_list(Ps),
+    Size = array:size(A),
+    lists:max(lists:map(fun(X) ->largest_consec_from_build(A, Size, 0, 1, 0, X) end, Ps)).
+
     
