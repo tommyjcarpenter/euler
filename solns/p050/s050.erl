eb6647348be4404e217f331e20e7bab2f99ef9e1
Tommy Carpenter <tommyjcarpenter@gmail.com>
2016-02-15 10:24:16 -0500

    fixed 50, near instant now

--- solns/p050/s050.erl
+++ solns/p050/s050.erl
@@ -9,26 +9,26 @@
 %Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
 -module(s050).
--export([solve/0]).
+-import(timer, [tc/2]).
+-export([solve/0, dosolve/0]).
 
 %returns the largest sublist (in terms of length) of L that sums to Target, or 0 if not possible
-largest_consec_from_build(A, Size, Start, N, Acc, Target) ->
-    if Start + N > Size-1  -> 0; %primes monotonucally increasing, so if H > T/2, then H + Hnext > Target 
+largest_consec_from_build(A, Size, Start, N, Acc) ->
+    if Start + N > Size-1  -> 0; %overrun, cant be formed as sum of consec primes
     true ->
         S = Acc + array:get(Start+N, A), 
-        %sublistsum(L, N), %I believe the first substring of primes that sums to the target is necessarily the longest. Because the primes are monotonically increasing, any substring summing to the same amount with a higher start index necessarily has les terms. So, return the first substring that sums to the target. 
-        if S == Target -> {N, Target};
-        true ->
-            if S > Target -> largest_consec_from_build(A, Size, Start+1, 1,   0, Target);
-            true ->          largest_consec_from_build(A, Size, Start,   N+1, S, Target)
-            end
+        if S > 999999 -> {N, Acc}; %largest_consec_from_build(A, Size, Start+1, 1,   0, Target);
+        true ->  largest_consec_from_build(A, Size, Start, N+1, S)
         end
     end.
 
-solve() ->
-    Ps = shared_euler:seive(999999),
-    A = array:from_list(Ps),
+dosolve() ->
+    A = array:from_list(shared_euler:seive(999999)),
+    Starts = lists:seq(0, 999999),
     Size = array:size(A),
-    lists:max(lists:map(fun(X) ->largest_consec_from_build(A, Size, 0, 1, 0, X) end, Ps)).
+    All_largest = lists:map(fun(X) ->largest_consec_from_build(A, Size, X, 0, 0) end, Starts),
+    lists:max([{X, Y} || {X, Y} <- All_largest, shared_euler:isprime(Y) == true]).
 
+solve() ->
+    timer:tc(s050, dosolve, []).
     
