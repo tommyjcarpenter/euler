c663ed1b4acc16d3d6a97c5b81707ccc35279c1d
Tommy Carpenter <tommyjcarpenter@gmail.com>
2018-08-28 15:29:16 -0400

    Do 71

--- eulermath.erl
+++ eulermath.erl
@@ -10,7 +10,9 @@
         istri/1, tri_n/1, ispent/1, ishex/1, is_palindrome/1, int_reverse/1, num_digits/1, nck/2, is_bouncy/1, is_increasing/1, is_decreasing/1,
         number_distinct_perms/1,
         gcd/2,
-        b10_to_2/1
+        gcd_memoized/2,
+        b10_to_2/1,
+        relative_primes/1
         ]).
 
 -spec intconcat(integer(), integer()) -> integer().
@@ -303,7 +305,43 @@ gcd(A, B) when B > A  -> gcd(B, A);
 gcd(A, B) when A rem B > 0 -> gcd(B, A rem B);
 gcd(A, B) when A rem B =:= 0 -> B.
 
+%use when you have overlapping subproblems computing the same GCD many times
+gcd_memoized(A, B) ->
+    C = erlang:get({A, B}),
+    case is_integer(C) of
+        true -> C;
+        false ->
+            R = case B > A of
+                    true -> gcd_memoized(B, A);
+                    false ->
+                        case A rem B > 0 of
+                            true -> gcd_memoized(B, A rem B);
+                            false -> B
+                        end
+            end,
+            erlang:put({A,B}, R),
+            R
+    end.
+
+
 b10_to_2(I) -> do_b10_to_2(I, []). %converts base 10 to base 2, %algorithm from http://chortle.ccsu.edu/assemblytutorial/zAppendixH/appH_4.html, CCSU represent!!!
     do_b10_to_2(0,BitList) -> digit_list_to_int(BitList);
     do_b10_to_2(Num, BitList) -> do_b10_to_2(trunc(Num / 2), [Num rem 2 | BitList]).
 
+
+relative_primes(N) ->
+    dofindrelativeprimes(lists:seq(2, N-2), N, [1, N-1]).
+dofindrelativeprimes([], _, SoFar) -> SoFar;
+dofindrelativeprimes([H|T], N, SoFar) ->
+    case H*H > N of
+        true ->
+            dofindrelativeprimes([], N, [H | SoFar] ++ T);
+        false ->
+            case N rem H == 0 of
+                true ->
+                    %H divides H
+                    dofindrelativeprimes([Y || Y <- T, Y rem H /= 0], N, SoFar);
+                false ->
+                    dofindrelativeprimes(T, N, [H | SoFar])
+            end
+    end.
---
+++ solns/p071/p071.erl
@@ -0,0 +1,26 @@
+-module(p071).
+-export([timesolve/0, solve/0]).
+
+timesolve() ->
+    erlang:display(timer:tc(p071, solve, [])).
+
+solve() ->
+    % After running this for smaller numbers than 1,000,000, call these maxes M, I noticed the following:
+    % the answer is of  the form N / D where D is very close to M and N is as high as possible.
+    % High as possible means the closest fraction to 3/7 given d, which means 3/7*d minus 1 (can't have it = 3/7, it wont be reduced!).
+    % When d is 999,999, this is 428,571)
+    % That tells me, the answer must be close to 999,999*(3/7) / 999,9999
+    % So we start from D=999,999 and iterate down
+    % Turns out 9999997 is the denominator
+    iterate(1000000).
+
+iterate(N) -> iterate(N-1,1,{666/666}).
+iterate(1,_, ClosestFrac) -> ClosestFrac;
+iterate(I,ClosestDelta, ClosestFrac) ->
+    erlang:display({I, ClosestFrac}),
+    MaxNum = trunc(math:ceil(3/7*I)) - 1,
+    NewDelta =  3/7 - MaxNum/I,
+    case NewDelta < ClosestDelta andalso eulermath:gcd(MaxNum, I) == 1 of
+        true -> iterate(I-1, NewDelta, {MaxNum, I});
+        false -> iterate(I-1, ClosestDelta, ClosestFrac)
+    end.
